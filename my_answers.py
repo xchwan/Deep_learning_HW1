8fabe499b06403e9a78b3591e5c4c3716d2a4266
Xch1 <r99222021@ntu.edu.tw>
2018-02-19 01:43:52 +0800

    Optimize to lower loss

---
+++ my_answers.py
@@ -0,0 +1,7 @@
+#########################################################
+# Set your hyperparameters here
+##########################################################
+iterations = 12000
+learning_rate = 0.07
+hidden_nodes = 30
+output_nodes = 1
